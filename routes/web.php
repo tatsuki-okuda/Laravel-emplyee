821f603e519adb396a536c479c7c9501cdd4f3e3
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-07-02 09:03:06 +0900

    user controller 追加

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('users', UserController::class);
